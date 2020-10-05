1b35b017bbeb56ebb945462f0364f10005cfe93c
Caleb Parris <calebparris@live.com>
2020-10-05 13:29:08 -0500

    Added the get route for all burgers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,13 @@ const express = require('express');
 const burger = require('../models/burger.js');
 
 const router = express.Router();
+
+router.get("/", function(req, res){
+    burger.selectAll(function(data){
+        let hbsObject = {
+            burgers: data
+        }
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});

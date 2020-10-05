98f0ad88b5073fa82c2a1b3562d97af63ecc916f
Caleb Parris <calebparris@live.com>
2020-10-05 15:26:11 -0500

    Added post route for new burgers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,3 +12,9 @@ router.get("/", function(req, res){
         res.render("index", hbsObject);
     });
 });
+
+router.post("/api/burgers", function(req, res){
+    burger.insertOne(["burger_name"], [req.body.burger], function(result){
+        res.json({ id: result.insertId });
+    });
+});

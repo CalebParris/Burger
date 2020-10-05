e8891267e1c3b20837436d9623df16c103d070bf
Caleb Parris <calebparris@live.com>
2020-10-05 16:46:14 -0500

    Added server functionality

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const routes = require("./controllers/burgers_controller.js");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log("Server is listening on: PORT " + PORT);
+});
